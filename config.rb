947bb75f7b60bc85b98b4f25b6128150363d58fd
kazuoyanagimoto <kazuo.yanagimoto@gmail.com>
2015-05-04 18:21:44 +0900

    Initial commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+add_import_path "bower_components/foundation/scss"
+
+require 'font-awesome-sass'
+require 'sass-globbing'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+dev_dir = "app"
+images_dir = dev_dir + "/images"
+javascripts_dir = dev_dir + "/scripts"
+fonts_dir = "../fonts"
---
+++ gulpfile.js
@@ -0,0 +1,163 @@
+// FOUNDATION FOR APPS TEMPLATE GULPFILE
+// -------------------------------------
+// This file processes all of the assets in the "client" folder, combines them with the Foundation for Apps assets, and outputs the finished files in the "build" folder as a finished app.
+
+// 1. LIBRARIES
+// - - - - - - - - - - - - - - -
+
+var $ = require('gulp-load-plugins')();
+var argv = require('yargs').argv;
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var rimraf = require('gulp-rimraf');
+var runSequence = require('run-sequence');
+
+// Check for --production flag
+var isProduction = (argv.production);
+// Default value for conditions
+var defaultVal = true;
+
+// 2. FILE PATHS
+// - - - - - - - - - - - - - - -
+
+var config = {
+  dev: 'app',
+  dist: 'build'
+};
+
+var paths = {
+  fonts: {
+    src: config.dev + '/fonts/*',
+    dest: config.dist + '/fonts'
+  },
+  images: {
+    src: config.dev + '/images/*',
+    dest: config.dist + '/imges'
+  },
+  scripts: {
+    src: config.dev +  '/scripts/*.js',
+    dest: config.dist + '/scripts'
+  },
+  styles: {
+    src: config.dev +  '/styles/*.scss',
+    dest: config.dist + '/styles'
+  }
+};
+
+// 3. TASKS
+// - - - - - - - - - - - - - - -
+
+// Cleans the build directory
+gulp.task('clean:server', function() {
+  return gulp.src(config.dist + '/*', { read: false })
+    .pipe(rimraf({ force: true }));
+});
+
+// Copies .html template
+gulp.task('copy:template', function() {
+  return gulp.src(config.app + '/*.html')
+    .pipe(gulp.dest(config.dist));
+});
+
+// Compile Sass to CSS using Compass
+// https://www.npmjs.com/package/gulp-compass
+gulp.task('compass', function() {
+  if(isProduction){
+    defaultVal = false;
+  }
+  return gulp.src(paths.styles.src)
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: './config.rb',
+      css: '.tmp/styles',
+      sass: 'app/styles',
+      style: (isProduction ? 'compressed' : 'nested'),
+      comments: defaultVal,
+      sourcemap: defaultVal
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+});
+
+// Gulp task Compass >Autoprefixer
+// Run gulp stylish with flag(--production)
+gulp.task('stylish', ['compass'], function() {
+  return gulp.src(paths.styles.dest + '/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe($.if(!isProduction, $.sourcemaps.write('./')))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(livereload());
+});
+
+// Concatenates files
+// https://www.npmjs.com/package/gulp-concat
+gulp.task('concat', function() {
+  return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe($.if(!isProduction, $.sourcemaps.write('./')))
+    .pipe(gulp.dest(paths.scripts.dest));
+});
+
+// Gulp task Concat >Uglify
+// Run gulp compress with flag(--production)
+gulp.task('compress', ['concat'], function() {
+  return gulp.src(paths.scripts.dest + '/*.js')
+    .pipe(plumber())
+    .pipe($.if(isProduction, $.uglify({
+      compress: {
+        drop_console: true
+      }
+    })))
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(livereload());
+});
+
+// Copies all .html in the app folder
+gulp.task('copy:template', function() {
+  return gulp.src(config.dev + '/*.html', {
+  // base: './app/'
+  })
+    .pipe(gulp.dest(config.dist));
+});
+
+// Gulp plugin to run a local webserver with LiveReload
+// https://www.npmjs.com/package/gulp-webserver
+gulp.task('server', function() {
+  return gulp.src(config.dist)
+    .pipe(webserver({
+    port: 8080,
+    host: 'localhost',
+    fallback: 'index.html',
+    livereload: true,
+    open: true
+    }));
+});
+
+// Watch, that actually is an endless stream
+// https://www.npmjs.com/package/gulp-watch
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(paths.scripts.src, ['compress']);
+  gulp.watch(paths.styles.src, ['stylish']);
+});
+
+// Dev task
+gulp.task('dev', ['initial task'], function() {
+  // runSequence('task');
+});
+// Serve task
+gulp.task('serve', function() {
+  runSequence('server', 'watch');
+});
+// Build task with flag(--production)
+gulp.task('build', ['clean:server'], function() {
+  runSequence('stylish', 'compress', 'copy:template');
+});
+// Default task
+gulp.task('default', ['clean:server'], function() {
+  runSequence('stylish', 'compress', 'copy:template', 'server', 'watch');
+});
